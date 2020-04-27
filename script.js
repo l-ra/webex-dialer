4bdfa0a0c505eb54e6eb059b4f138a27ddfc9c93
lra <lra@r73.info>
2020-04-27 11:58:50 +0200

    wip

---
+++ script.js
@@ -0,0 +1,63 @@
+window.addEventListener("load",()=>{
+    document.querySelector("#ok").addEventListener("click",()=>{
+        const ent = {
+            kod:document.querySelector("#kod").value.replace(/[^0-9]+/g,""),
+            nazev:s(document.querySelector("#nazev").value),
+            telefon:document.querySelector("#telefon").value,
+        }
+        const stored = loadStored();
+        stored[ent.nazev]=ent
+        saveStored(stored)
+        displayStored(stored);
+    })
+    const hash = window.location.hash.substring(1)
+    if (hash){
+        const decodedHash = decodeURIComponent(hash)
+        //console.log(decodedHash)
+        const fromHash = JSON.parse(decodedHash)
+        const stored = loadStored()
+        saveStored({...stored,...fromHash})
+    }
+    displayStored()
+})
+
+function loadStored(){
+    return ( localStorage['stored'] && JSON.parse(localStorage['stored'] ) ) || {}
+}
+
+function saveStored(s){
+    localStorage['stored']=JSON.stringify(s,null,"  ")
+}
+
+function displayStored(storedIn){
+    const stored = storedIn || loadStored();
+    const out = document.querySelector("#out");
+    out.innerHTML=""
+    for ( let key of Object.keys(stored) ){
+        const ent = stored[key]
+        const href = `tel:${ent.telefon};${ent.kod}${encodeURI('*')}`
+        const display = `${ent.nazev}: ${ent.telefon};${ent.kod}*`
+        const li = document.createElement("li");
+        li.classList.add("list-group-item")
+        li.innerHTML = `<a href="${s(href)}">${s(display)}</a> <button onclick="removeEntry(event)" data-nazev="${s(ent.nazev)}">🗑️</button>`
+        out.appendChild(li)
+    }
+    const hash =encodeURIComponent(JSON.stringify(stored))
+
+    window.location.hash=hash
+}
+
+//sanitize
+function s(x){
+    let val = String(x)
+    return val.replace(/["'<>]/g,"🥱");
+}
+
+function removeEntry(ev){
+    const nazev = ev.target.dataset.nazev
+    //console.log("remove stored:",nazev)
+    const stored = loadStored();
+    delete stored[nazev]
+    saveStored(stored)
+    displayStored(stored)
+}
